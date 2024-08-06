67277e52ae66e44dbc881d248b329828d6143c66
nooby6 <giftaudrine1@gmail.com>
2024-08-06 18:03:57 +0300

    Update navbar component with new class names and logo source

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
-import './navbar.css'
+import './navbar.css';
 
-//BEM- block element modifier
+// BEM - Block Element Modifier
+
 const Navbar = () => {
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
-          <img src="{logo}" alt="Logo" />
+          <img src={logo} alt="logo" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
