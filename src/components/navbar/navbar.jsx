1dd98d31a9d83755db4fa81975ee0174b0d3366f
nooby6 <giftaudrine1@gmail.com>
2024-08-07 23:21:53 +0300

    Update navbar component with consistent naming convention for toggleMenu state variable

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,6 +5,16 @@ import './navbar.css';
 
 // BEM - Block Element Modifier
 
+const Menu = () => (
+    <>
+      <p><a href="#home">Home</a></p>
+      <p><a href="#wgpt3">What is GPT3?</a></p>
+      <p><a href="#possibility">Open AI</a></p>
+      <p><a href="#features">Case Studies</a></p>
+      <p><a href="#blog">Library</a></p>
+    </>
+)
+
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
