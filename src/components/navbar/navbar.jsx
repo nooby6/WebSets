2156a0f132e45fc2d57762e4a573cbd41a50c977
nooby6 <giftaudrine1@gmail.com>
2024-08-08 17:21:30 +0300

    Update navbar component with lazy loading for images

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -23,14 +23,9 @@ const Navbar = () => {
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt="logo" />
-          <Menu />
         </div>
         <div className='gpt3__navbar-links_container'>
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+        <Menu />
         </div>
       </div>
       <div className='gpt3__navbar-sign'>
