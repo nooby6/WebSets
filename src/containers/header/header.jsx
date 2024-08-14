2fb3f2a47c7a3d2b501486864b7d6c2683ed4a26
nooby6 <giftaudrine1@gmail.com>
2024-08-14 12:24:14 +0300

    chore: Update header component with responsive design and gradient text

--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <div className='gpt3__header section__padding' id='home'>
      <div className='gpt3__header-content'>
-      <h1 className='gradient__text'></h1>
+      <h1 className='gradient__text'>Let’s Build Something
+amazing with GPT-3
+OpenAI</h1>
      </div>
     </div>
   )
